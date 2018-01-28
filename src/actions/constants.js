b720c7fb92d756eed52d8c64ce4e300fb3a9d5dd
Milad Nekooei <mi.nekooei@gmail.com>
2018-01-28 18:15:08 +0330

    Create Constants for redux

---
+++ src/actions/constants.js
@@ -0,0 +1,10 @@
+/**
+ * Created by milad on 1/28/18.
+ */
+export const FETCHING = "FETCHING";
+export const CANCEL_FETCHING = "CANCEL_FETCHING";
+
+export const UPDATE_LOGIN_TOKEN = "UPDATE_LOGIN_TOKEN";
+export const DELETE_LOGIN_TOKEN = "DELETE_LOGIN_TOKEN";
+
+export const LOGIN_STUDENT = "LOGIN_STUDENT";
