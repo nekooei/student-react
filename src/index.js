f4949e3461910c3f7bcf0321bb9e3615513bed90
Milad Nekooei <mi.nekooei@gmail.com>
2018-01-28 14:57:24 +0330

    Make RTL direction with themeProvider and JSS co-working with react-jss package

--- src/index.js
+++ src/index.js
@@ -3,11 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import {MuiThemeProvider, createMuiTheme} from 'material-ui/styles';
 import registerServiceWorker from './registerServiceWorker';
+import { create } from 'jss';
+import rtl from 'jss-rtl';
+import JssProvider from 'react-jss/lib/JssProvider';
+import { createGenerateClassName, jssPreset } from 'material-ui/styles';
+
+const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
+const generateClassName = createGenerateClassName();
+
+
+
+const theme = createMuiTheme({
+  direction : 'rtl'
+});
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <JssProvider jss={jss} generateClassName={generateClassName}>
+        <App />
+      </JssProvider>
+    </MuiThemeProvider>
+
   </BrowserRouter>
   , document.getElementById('root')
 );
