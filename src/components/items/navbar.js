7aaaa86fd95b57a15cb37d04cd6881ce4fa5ce38
Milad Nekooei <mi.nekooei@gmail.com>
2018-01-28 18:30:53 +0330

    Create Simple NavBar with progress bar

---
+++ src/components/items/navbar.js
@@ -0,0 +1,58 @@
+/**
+ * Created by milad on 1/28/18.
+ */
+import React, {Component} from 'react';
+//import {connect} from 'react-redux';
+import {
+  AppBar,
+  Toolbar, Typography,
+  LinearProgress
+} from 'material-ui';
+import {withStyles} from 'material-ui/styles';
+
+const styles = {
+  root: {
+    width: '100%',
+  },
+  flex: {
+    flex: 1,
+  }
+}
+
+class NavBar extends Component {
+  state = {
+    fetching : false
+  };
+
+  render() {
+    const {classes} = this.props;
+    return (
+      <div className={classes.root}>
+        <AppBar position={'fixed'}>
+          <Toolbar>
+            <Typography type="title" color="inherit"  classes={classes.flex}>
+              دانش آموز
+            </Typography>
+          </Toolbar>
+          {this.state.fetching ? (
+            <LinearProgress mode='query' color='primary'/>
+          ) : undefined}
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+
+/*
+function mapStateToProps(state) {
+  return {
+    fetching : state.fetching
+  };
+}
+function mapDispatchToProps(dispatch) {
+  return dispatch;
+}
+*/
+
+export default withStyles(styles)(NavBar);
