9cf6f9bdb8f3c99f067f721ed403f44db1ce3256
Milad Nekooei <mi.nekooei@gmail.com>
2018-01-29 21:31:12 +0330

    student actions

--- src/actions/student.js
+++ src/actions/student.js
@@ -1,3 +1,26 @@
 /**
  * Created by milad on 1/28/18.
  */
+import {
+  SET_STUDENT_INFO,
+  REMOVE_STUDENT_INFO
+} from './constants';
+import {
+  getStudentInfo
+} from '../utils/api';
+
+export const setUserInfo = () => dispatch => (
+  getStudentInfo()
+    .then(response => (
+      dispatch({
+        type: SET_STUDENT_INFO,
+        payload: response.data
+      })
+    ))
+);
+
+export const removeUserInfo = () => dispatch => (
+  dispatch({
+    type: REMOVE_STUDENT_INFO
+  })
+);
