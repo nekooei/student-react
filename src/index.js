3b92f17717d77e707b427fcdbacac4d1d18e671a
Milad Nekooei <mi.nekooei@gmail.com>
2018-01-30 11:49:32 +0330

    Apply redux-store in application using provider

--- src/index.js
+++ src/index.js
@@ -9,11 +9,22 @@ import { create } from 'jss';
 import rtl from 'jss-rtl';
 import JssProvider from 'react-jss/lib/JssProvider';
 import { createGenerateClassName, jssPreset } from 'material-ui/styles';
+import {createStore, applyMiddleware, compose} from 'redux';
+import reducers from './reducers';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
 const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
 const generateClassName = createGenerateClassName();
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const store = createStore(
+  reducers,
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
+);
 
 const theme = createMuiTheme({
   direction : 'rtl'
@@ -23,7 +34,9 @@ ReactDOM.render(
   <BrowserRouter>
     <MuiThemeProvider theme={theme}>
       <JssProvider jss={jss} generateClassName={generateClassName}>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </JssProvider>
     </MuiThemeProvider>
 
