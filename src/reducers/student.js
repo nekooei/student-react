3420989f3a14a4d0e0e045d133b340e53d04c72a
Milad Nekooei <mi.nekooei@gmail.com>
2018-01-30 11:26:30 +0330

    Create reducer fro student

---
+++ src/reducers/student.js
@@ -0,0 +1,18 @@
+/**
+ * Created by milad on 1/30/18.
+ */
+import {
+  SET_STUDENT_INFO,
+  REMOVE_STUDENT_INFO
+} from '../actions/constants';
+
+const initState = {};
+
+export default (state = initState, action) => {
+  switch (action.type){
+    case SET_STUDENT_INFO:
+      return action.payload;
+    case REMOVE_STUDENT_INFO:
+      return {};
+  }
+}
