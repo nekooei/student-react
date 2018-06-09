d53cec69b195454eaf512a3a0347ea4c054800c4
Milad Nekooei <mi.nekooei@gmail.com>
2018-06-09 20:03:17 +0430

    Create hide snackBar action creator

--- src/actions/ui.js
+++ src/actions/ui.js
@@ -1,8 +1,14 @@
-import {UI_SET_SNACK_BAR} from './constants';
+import {UI_HIDE_SNACK_BAR, UI_SET_SNACK_BAR} from './constants';
 
 export const setSnackBar = (message) => (
   {
     type: UI_SET_SNACK_BAR,
     payload: message
+  }
+);
+
+export const hideSnackBar = () => (
+  {
+    type: UI_HIDE_SNACK_BAR
   }
 );
