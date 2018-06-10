35be6733574b5cf82135f71b7944c14c8ca18f46
Milad Nekooei <mi.nekooei@gmail.com>
2018-06-10 20:05:36 +0430

    Create default Payment Card component

---
+++ src/components/items/Payment.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Payment extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+Payment.propTypes = {};
+Payment.defaultProps = {};
+
+export default Payment;
